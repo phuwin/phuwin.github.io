5d9a419f7db42a96cf13a7eda92ddebe4dc5e084
phuwin <ngp130895@gmail.com>
2016-10-15 15:51:35 +0300

    clean code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 $(function(){	
-	displayPhu();
-
 	displayContent();
 });
 
-function displayPhu () {
-	$('#phu').show();
-	$('#phu').animate({ opacity: 0 }, 0);
-	$('#phu').animate({ opacity: 1 }, 2000);
-}
 
 function displayContent() {
 	$('#content').show();

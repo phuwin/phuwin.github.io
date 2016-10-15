b41e67d6fe3691374fd30d053061af955a746b37
phuwin <ngp130895@gmail.com>
2016-10-15 14:55:15 +0300

    add contents

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-$(function(){
-	console.log('ready');
-})
+$(function(){	
+	displayPhu(displayContent);
+
+
+});
+
+function displayPhu (callback) {
+	$('#phu').show();
+	$('#phu').animate({ opacity: 0 }, 0);
+	$('#phu').animate({ opacity: 1, top: "-1000px" }, 500);
+	if (callback) callback();
+}
+
+function displayContent() {
+	$('#content').show();
+	$('#content').animate({ opacity: 0 }, 0);
+	$('#content').animate({ opacity: 1, top: "-10px" }, 500);
+}

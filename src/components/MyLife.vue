c886fbb7afca0368c5acd3480c26afc9e537544f
Phu Nguyen <ngp130895@gmail.com>
2018-10-16 15:31:16 +0300

    add mylife components to frontpage

--- src/components/MyLife.vue
+++ src/components/MyLife.vue
@@ -1,31 +1,53 @@
 <template>
-
+  <v-container fluid white id="my-events">
+    <v-container>
+      <div data-aos="fade" data-aos-duration="2000">
+        <v-layout v-for="(event,index) in myEvents.slice().reverse()" :key="index" row>
+            <v-flex xs12 md6 class="life-event-container" :class= "[(index%2===1)? 'right' : '']">
+              <my-life-event :event = "event"></my-life-event>
+            </v-flex>
+        </v-layout>
+      </div>
+    </v-container>
+  </v-container>
 </template>
 <script>
-import moment from 'moment'
 import LifeEvent from '@/classes/LifeEvent'
+import MyLifeEvent from '@/components/MyLifeEvent'
 export default {
   name: 'MyLife',
   props: [],
-  components: {},
+  components: {
+    'my-life-event': MyLifeEvent
+  },
   data () {
     return {
       myEvents: [
-        new LifeEvent('Born', '', moment('1995-08-13').toDate(), 'Hai Phong, Vietnam'),
-        new LifeEvent('Moved', 'Moved to Saigon', moment('1997-12-31').toDate(), ''),
-        new LifeEvent('Studied abroad', 'Europe', moment('2013-09-01').toDate(), 'Hai Phong, Vietnam'),
-        new LifeEvent('Graduated', 'Average of 4.25', moment('2016-12-20').toDate(), 'Lahti University of Applied Sciences, Finland'),
-        new LifeEvent('Software Developer', 'Making websites and mobile applications', moment('2015-09-01').toDate(), -1)
+        new LifeEvent('Born', '', '13th August, 1995', 'Hai Phong, Vietnam'),
+        new LifeEvent('Moved to Saigon', '', '1998', ''),
+        new LifeEvent('Studied abroad', 'University of Applied Sciences', '2013', 'Lahti, Finland'),
+        new LifeEvent('Graduated', 'Average of 4.25', 'December 2016', 'Lahti University of Applied Sciences'),
+        new LifeEvent('Software Developer', 'Making websites and mobile applications', 'August 2015', 'SuperApp Oy', 'present')
       ]
     }
-  },
-  mounted () {
-    console.log(this.$data.myEvents)
-  },
-  methods: {}
+  }
 }
 </script>
 <style scoped>
+
+@media (min-width: 960px) {
+  .life-event-container {
+    text-align:right;
+    padding:10px;
+  }
+  .right {
+    margin-left:50%;
+    text-align:left;
+  }
+  .white {
+    color:black;
+  }
+}
 
 </style>
 
---
+++ src/components/MyLifeEvent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <div>
+        <b>{{event.time}} </b>
+        <b v-if="event.endTime">- {{event.endTime}}</b>
+      </div>
+      <div>
+        {{event.title}}
+      </div>
+      <div>
+        {{event.text}}
+      </div>
+      <div>
+        {{event.place}}
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MyLifeEvent',
+  props: {
+    event: Object
+  }
+}
+</script>

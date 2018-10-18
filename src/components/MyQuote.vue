366086901b92de7df158456c1483365eb6522d70
Phu Nguyen <ngp130895@gmail.com>
2018-10-18 10:23:14 +0300

    responsiveness to myquote

--- src/components/MyQuote.vue
+++ src/components/MyQuote.vue
@@ -4,11 +4,9 @@
       <v-layout row align-center>
         <v-flex sm12 md6 offset-md4 text-xs-right>
           <div data-aos="fade" data-aos-duration="2000">
-            <div class="headline">
-              <i>"Clean code is love, clean code is life"</i>
-            </div>
-            <b>- Phu
-            </b>
+            <i>"Clean code is love,<br> clean code is life"</i>
+            <br>
+            <b>- Phu</b>
           </div>
         </v-flex>
       </v-layout>
@@ -23,6 +21,14 @@
 <style scoped>
   #my-quote >>> .row {
     height: 300px;
+  }
+  #my-quote >>> i {
+    font-size:40px;
+  }
+  @media screen and (max-width :470px){
+    #my-quote >>> i {
+      font-size: 7vw;
+    }
   }
 </style>
 
